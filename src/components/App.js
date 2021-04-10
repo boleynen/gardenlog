79f370ebeeb70b5d64d47e236304877641f07115
boleynen <58037455+boleynen@users.noreply.github.com>
2021-04-10 17:50:11 +0200

    Added working Signup form

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Signup  from './Signup';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../contexts/AuthContext';
+
+
+const App = () =>{
+    return(
+        <AuthProvider>
+            <Container style={{minHeight:"100vh"}} className="d-flex align-items-center justify-content-center">
+                <div className="w-100" style={{maxWidth: "400px"}}>
+                    <Signup />
+                </div>
+            </Container>
+        </AuthProvider>
+    )
+}
+
+export default App;

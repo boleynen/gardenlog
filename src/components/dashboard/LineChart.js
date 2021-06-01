f70b5a49d8b0d608a764fbcf8a92e78715bdbdbc
boleynen <58037455+boleynen@users.noreply.github.com>
2021-06-01 15:53:13 +0200

    added todays date as default state above graph data

--- src/components/dashboard/LineChart.js
+++ src/components/dashboard/LineChart.js
@@ -2,63 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Line } from 'react-chartjs-2'
 
 function LineChart(props){
-    let [waterData, setWaterData] = useState(props.waterData)
-    let [graphHours, setGraphHours] = useState([]);
-    let [data, setData] = useState({
-        labels: ["02:00","04:00","06:00","08:00","10:00","12:00","14:00","16:00","18:00","20:00","22:00","24:00"],
-        datasets: [{
-            label: '',
-            data: [],
-            borderColor: [],
-            tension: 0.3,
-            borderWidth: 4,
-        }]
-    })
-    let color = props.color;
-
-    let hoursArray = [];
-
-    const unixToTime = (u) =>{
-        var timestamp = u; // UNIX timestamp in seconds
-        var xx = new Date();
-        xx.setTime(timestamp*1000); // javascript timestamps are in milliseconds
-        return(xx.toLocaleTimeString('nl-NL')); // the Day
-    }
-    
-    useEffect(() => {
-        
-        var d = new Date();
-        d.setHours(0,0,0,0); // last midnight
-        let lastMidnight = d.getTime()/1000;
-
-        Object.keys(waterData).map((key) => {
-            if(key > lastMidnight ){
-                let time = unixToTime(key)
-                time = time.slice(0,5)
-                if(time == "02:00" || time == "04:00" || time == "06:00" || time == "08:00" || time == "10:00" || time == "12:00" || time == "14:00" || time == "16:00" || time == "18:00" || time == "20:00" || time == "22:00" || time == "24:00"){
-                    hoursArray.push(time)
-                }
-            }
-        })
-
-        hoursArray = [...new Set(hoursArray)]
-
-        data = {
-            labels: hoursArray,
-            datasets: [{
-                label: 'Vochtigheid',
-                // data: props.waterData,
-                data: [1,2,3,4,5,6,7,8,9,10],
-                borderColor: color,
-                tension: 0.3,
-                borderWidth: 4,
-            }]
-        }
-
-        setData(data)
-
-    }, [])
-
+    let data = props.data;
     return(
         <>
         {data != "" ? (
--- src/components/dashboard/SensorDetailsGraph.js
+++ src/components/dashboard/SensorDetailsGraph.js
@@ -1,16 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LineChart from './LineChart'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import './SensorDetailsGraph.scss'
+
+// vraag dag en datum van vandaag op
+const date = new Date();
+let weekday = date.toLocaleString('nl-NL', { weekday: 'long' });
+weekday = weekday[0].toUpperCase() + weekday.substring(1);
+let day = date.toLocaleString('nl-NL', { day: 'numeric' });
+let month = date.toLocaleString('nl-NL', { month: 'long' });
+const today = weekday + ' ' + day + ' ' + month;
+
+// UNIX naar timestamp
+const unixToTime = (u) =>{
+  var timestamp = u; // UNIX timestamp in seconds
+  var xx = new Date();
+  xx.setTime(timestamp*1000); // javascript timestamps are in milliseconds
+  return(xx.toLocaleTimeString('nl-NL')); // the Day
+}
 
 function SensorDetailsGraph(props) {
     let waterData = props.waterData;
     let color = props.color;
-  
-    // console.log(waterData)
-    return (
+    let [data, setData] = useState([])
+    let [selectedDay, setSelectedDay] = useState(today)
+    let hoursArray = [];
+
+    
+    useEffect(() => {
+      // vraag laatste tijd van de laatste middernacth op
+      var d = new Date();
+      d.setHours(0,0,0,0);
+      let lastMidnight = d.getTime()/1000;
+
+      // loop over timestamps in database, en kijk welke van vandaag zijn a.h.v laatste middernacht
+      Object.keys(waterData).map((key) => {
+          if(key > lastMidnight ){
+              let time = unixToTime(key)
+              time = time.slice(0,5)
+              if(time == "02:00" || time == "04:00" || time == "06:00" || time == "08:00" || time == "10:00" || time == "12:00" || time == "14:00" || time == "14:00" || time == "18:00" || time == "20:00" || time == "22:00" || time == "24:00"){
+                  hoursArray.push(time)
+              }
+          }
+      })
+
+      // verwijderd dubbele waardes
+      hoursArray = [...new Set(hoursArray)]
+
+      data = {
+          labels: hoursArray,
+          datasets: [{
+              label: 'Vochtigheid',
+              // data: props.waterData,
+              data: [1,2,3,4,5,6,7,8,9,10],
+              borderColor: color,
+              tension: 0.3,
+              borderWidth: 4,
+          }]
+      }
+
+      setData(data)
+
+
+
+  }, [])
+
+  return (
       <>
       <div className={"sensorDetailsGraph"}>
-         <LineChart waterData={waterData} color={color}/>
+        <div className={"sensorDetailsGraph__day"}>
+          <FontAwesomeIcon icon={faAngleLeft} />
+          <p>{selectedDay}</p>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </div>
+         <LineChart data={data}/>
       </div>
       </>
     );

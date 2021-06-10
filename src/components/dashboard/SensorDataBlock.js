a27412f7038d176640e77ac4feaf1602014cb60c
boleynen <58037455+boleynen@users.noreply.github.com>
2021-06-10 19:47:24 +0200

    added 'add calendarnote' to dashboard that sends note to firebase

--- src/components/dashboard/SensorDataBlock.js
+++ src/components/dashboard/SensorDataBlock.js
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import ButtonMedium from './../navigation/ButtonMedium'
 import SensorCardList from './SensorCardList';
-import ButtonMedium from '../navigation/ButtonMedium';
+import ModalHeader from 'react-bootstrap/esm/ModalHeader';
+import app from "./../../firebase"
 import './SensorDataBlock.scss';
+
 function SensorDataBlock(props) {
   // Declare a new state variables
   const sensorData = props.sensorData;
+
+  const [show, setShow] = useState(false)
+
+  const userId = app.auth().currentUser.uid
+  const database = app.database();
+
+  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false)
+
+  const titleRef = useRef()
+  const descRef = useRef()
+
+  function handleSubmit(e){
+    e.preventDefault();
+    let unixMs = new Date().getTime()/1000
+    let unix = Math.floor(unixMs)
+    
+    function setUserNote(date, inputTitle, inputDesc){
+      database.ref(`user_notes/` + userId + "/" + date).set({
+        title: inputTitle,
+        desc: inputDesc
+      })
+    }
+
+    setUserNote(unix, titleRef.current.value, descRef.current.value)
+
+  }
+
   return (
+    <>
     <div className={'sensorDataBlock'}>
       <SensorCardList   key={props.sensorData} 
                         sensorData={props.sensorData} 
@@ -17,8 +52,37 @@ function SensorDataBlock(props) {
             </h3>
           );
         })}
-      <ButtonMedium buttonText={'Notitie toevoegen'} buttonLink={''} />
+      <Button className="modalButton" variant="primary" onClick={handleShow}>
+        Kalendernotitie toevoegen
+      </Button>
     </div>
+
+    <Modal show={show} onHide={handleClose} aria-labelledby="contained-modal-title-vcenter" centered>
+      <Form onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title >Kalendernotitie toevoegen</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="" id="title">
+            <Form.Label>Titel</Form.Label>
+            <Form.Control type="title" ref={titleRef}/>
+          </Form.Group>
+          <Form.Group className="" id="desc">
+            <Form.Label>Beschrijving (optioneel)</Form.Label>
+            <Form.Control as="textarea" ref={descRef}/>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="modalButton dark" variant="secondary" onClick={handleClose}>
+            Annuleer
+          </Button>
+          <Button className="modalButton small" variant="primary" onClick={handleClose} type="submit">
+            Opslaan
+          </Button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
+    </>
   );
 }
 

9a51eb778c327f4b8f58c6c572b2da6017f448dc
CharlotteJeuris <charlottejeuris@hotmail.com>
2021-06-14 18:34:39 +0200

    kalender eerste deel

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,11 +1,18 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
 import TopNav from './navigation/TopNav';
 import Navigation from './navigation/BottomNav';
-import Loader from "./Loader"
-import './Calendar.scss'
+import Loader from "./Loader";
+import './Calendar.scss';
+import Calender from 'react-calendar';
+//import 'react-calendar/dist/Calendar.css';
+import Moment from 'react-moment';
 
-function Calendar() {
+export default function Calendar() {
     const [loading, setLoading] = useState(false)
+    const [dateState, setDateState] = useState(new Date())
+    const changeDate = (e) => {
+        setDateState(e)
+    }
 
     useEffect(() => {
         setLoading(true)
@@ -23,18 +30,18 @@ function Calendar() {
     return (
         <>
             <div className="content-wrapper">
-                <TopNav  pageTitle={"Kalender"}/>
-                <div className="content calendar">
-                    {loading === false ? (
-                        <h1>hier komt kalender content</h1>
-                    ) : (
-                        <Loader/>
-                    )}
+                <TopNav className="title"  pageTitle={"Kalender"}/>
+                <div className="calender">
+                    <Calender
+                        value={dateState}
+                        onChange={changeDate}
+                    />
+                </div>
+                <div className="dateName">
+                    <Moment format="dddd DD/MM">{dateState}</Moment>
                 </div>
                 <Navigation/>
             </div>
         </>
     )
 }
-
-export default Calendar

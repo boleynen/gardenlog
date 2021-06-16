77d6ecd1f5d00163105b5fcd53ce77d4f779f85e
boleynen <58037455+boleynen@users.noreply.github.com>
2021-06-16 15:35:02 +0200

    added charlotte calendar code

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,21 +6,46 @@ import './Calendar.scss';
 import Calender from 'react-calendar';
 //import 'react-calendar/dist/Calendar.css';
 import Moment from 'react-moment';
+import app from "../firebase"
+import unixToDate from "../functions/unixToDate";
+import {forEach} from "react-bootstrap/ElementChildren";
+
 
 export default function Calendar() {
     const [loading, setLoading] = useState(false)
     const [dateState, setDateState] = useState(new Date())
-    const changeDate = (e) => {
-        setDateState(e)
+    const database = app.database();
+    const userId = app.auth().currentUser.uid
+
+    function changeDate (e){
+        setDateState(e);
+        console.log(e);
     }
 
-    useEffect(() => {
+    let databaseRef = database.ref(`user_notes/` + userId + "/")
+    let timeSlots = [];
+    let calendarNotes = [];
+
+    function showNotes() {
+        let j;
+        for (j = 0; j < calendarNotes.length; j++) {
+            document.getElementById('calendarNotes').innerHTML += '<p class="item">'  + calendarNotes[j].title + '<br/>'  + calendarNotes[j].desc + '</p>'
+        }
+    }
+
+    useEffect((children, func) => {
         setLoading(true)
-        // roep hieronder kalender aan
+        // notities opzoeken
+        databaseRef.on('value', (data) =>{
+            timeSlots = data.val()
+        })
 
-
-
-
+        let i = 0;
+        for(const[time, calenderNotesDetails] of Object.entries(timeSlots)){
+            calendarNotes[i] = calenderNotesDetails
+            i++;
+        }
+       showNotes()
 
         // laat de timer vanonder aan de useEffect staan!
         setTimeout(() => {
@@ -39,6 +64,8 @@ export default function Calendar() {
                 </div>
                 <div className="dateName">
                     <Moment format="dddd DD/MM">{dateState}</Moment>
+                </div>
+                <div id="calendarNotes">
                 </div>
                 <Navigation/>
             </div>
